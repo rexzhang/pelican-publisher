40a4d492f0b7302d15408d5b1abc56d45b1db493
Rex Zhang <rex.zhang@gmail.com>
2021-12-27 22:37:45 +0800

    Bump version to 0.4.0

--- pelican_publisher/pelican_publisher/__init__.py
+++ pelican_publisher/pelican_publisher/__init__.py
@@ -9,7 +9,7 @@ from .celery import app as celery_app
 __all__ = ("celery_app", "__version__")
 
 __name__ = "Pelican Publisher"
-__version__ = "0.3.11"
+__version__ = "0.4.0"
 
 __project_url__ = "https://github.com/rexzhang/pelican-publisher"
 __docker_url__ = "https://hub.docker.com/r/ray1ex/pelican-publisher"
