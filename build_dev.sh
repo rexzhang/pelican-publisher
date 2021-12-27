194c5933f7028d5b17d9626396d705a5194bb586
Rex Zhang <rex.zhang@gmail.com>
2021-12-27 22:37:03 +0800

    Update build script

--- build_dev.sh
+++ build_dev.sh
@@ -3,8 +3,7 @@ docker container rm pelican-publisher
 docker image rm pelican-publisher
 
 docker build -t ray1ex/pelican-publisher .
-# shellcheck disable=SC2046
-docker rmi -f $(docker images -qa -f "dangling=true")
+docker image prune -f
 
 docker run -dit -p 127.0.0.1:8000:8000 -v /tmp:/pp-output -e DEBUG=true \
   --name pelican-publisher ray1ex/pelican-publisher
